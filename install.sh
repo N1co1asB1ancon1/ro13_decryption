fb9eb3e2b6813a54d4c1a1c183f206f80b14c28f
N1co1asB1ancon1 <88852049+N1co1asB1ancon1@users.noreply.github.com>
2022-02-11 09:19:13 -0300

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# copy this line to your bashrc (or zshrc) 
+# alias rot13="tr 'A-Za-z' 'N-ZA-Mn-za-m'"
+#because the line bellow might not work depending on the enviroment
+echo 'alias rot13="tr 'A-Za-z' 'N-ZA-Mn-za-m'"' >> ~.bashrc
